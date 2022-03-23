540bd5dde8f66831e2bb8570850fa524e7dbab73
LEEKANGTO <njy1350@naver.com>
2022-03-23 16:00:12 +0900

    ImageViewer2-Winform first

---
+++ ImageViewer2WinForm/ImageViewer2WinForm/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ImageViewer2WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.statusStripMain = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelVer = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelPos = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelPixelValue = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            this.statusStripMain.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxImage.Location = new System.Drawing.Point(6, 49);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(1043, 451);
+            this.pictureBoxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxImage.TabIndex = 0;
+            this.pictureBoxImage.TabStop = false;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(6, 6);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(181, 33);
+            this.buttonLoad.TabIndex = 1;
+            this.buttonLoad.Text = "Load(&L)...";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // statusStripMain
+            // 
+            this.statusStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelVer,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabelPos,
+            this.toolStripStatusLabelPixelValue,
+            this.toolStripStatusLabel1});
+            this.statusStripMain.Location = new System.Drawing.Point(0, 506);
+            this.statusStripMain.Name = "statusStripMain";
+            this.statusStripMain.Size = new System.Drawing.Size(1055, 24);
+            this.statusStripMain.TabIndex = 14;
+            this.statusStripMain.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelVer
+            // 
+            this.toolStripStatusLabelVer.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabelVer.Name = "toolStripStatusLabelVer";
+            this.toolStripStatusLabelVer.Size = new System.Drawing.Size(29, 19);
+            this.toolStripStatusLabelVer.Text = "Ver";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(470, 19);
+            this.toolStripStatusLabel2.Spring = true;
+            // 
+            // toolStripStatusLabelPos
+            // 
+            this.toolStripStatusLabelPos.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabelPos.Name = "toolStripStatusLabelPos";
+            this.toolStripStatusLabelPos.Size = new System.Drawing.Size(30, 19);
+            this.toolStripStatusLabelPos.Text = "Pos";
+            // 
+            // toolStripStatusLabelPixelValue
+            // 
+            this.toolStripStatusLabelPixelValue.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabelPixelValue.Name = "toolStripStatusLabelPixelValue";
+            this.toolStripStatusLabelPixelValue.Size = new System.Drawing.Size(41, 19);
+            this.toolStripStatusLabelPixelValue.Text = "Value";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(470, 19);
+            this.toolStripStatusLabel1.Spring = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.OutsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxImage, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonLoad, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1055, 506);
+            this.tableLayoutPanel1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 530);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStripMain);
+            this.Name = "Form1";
+            this.Text = "ImageViewer2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            this.statusStripMain.ResumeLayout(false);
+            this.statusStripMain.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.StatusStrip statusStripMain;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelPos;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelPixelValue;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelVer;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ ImageViewer2WinForm/ImageViewer2WinForm/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace ImageViewer2WinForm
+{
+    public partial class Form1 : Form
+    {
+        private int x1;
+        private int y1;
+        private bool ImageLoaded = false;
+        private Mat image = null;
+        private MatType imageType;
+        private int red;
+        private int green;
+        private int blue;
+        private int gray;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitControl();
+            InitMouseMoveEvent();
+        }
+
+        public void InitControl()
+        {
+            toolStripStatusLabelVer.Text = String.Format("Ver: {0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+        }
+
+        public void InitMouseMoveEvent()
+        {
+            this.pictureBoxImage.MouseMove += PictureBox_MouseMove;
+        }
+
+        private void PictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (ImageLoaded)
+            {
+                this.pictureBoxImage.Cursor = GetPictureBoxRect(this.pictureBoxImage).Contains(e.Location) ? Cursors.Cross : Cursors.Default;
+                ConvertPictureBoxCoordinates(this.pictureBoxImage, out this.x1, out this.y1, e.X, e.Y);
+                this.toolStripStatusLabelPos.Text = String.Format("X: {0} Y: {1} ", x1, y1);
+                if (imageType == MatType.CV_8UC3)
+                {
+                    GetImgPixelRGBValue(out this.red, out this.green, out this.blue, x1, y1);
+                    toolStripStatusLabelPixelValue.Text = String.Format("R: {0} G: {1} B: {2}", red, green, blue);
+                }
+                else if (imageType == MatType.CV_8UC1)
+                {
+                    GetImgPixelGrayValue(out this.gray, x1, y1);
+                    toolStripStatusLabelPixelValue.Text = String.Format("Gray: {0}", gray);
+                }
+
+            }
+        }
+
+        private Rectangle GetPictureBoxRect(PictureBox box)
+        {
+            System.Drawing.Size si = box.Size;
+            System.Drawing.Size sp = box.ClientSize;
+            float ri = 1f * si.Width / si.Height;
+            float rp = 1f * sp.Width / sp.Height;
+            if (rp > ri)
+            {
+                int width = si.Width * sp.Height / si.Height;
+                int left = (sp.Width - width) / 2;
+                return new Rectangle(left, 0, width, sp.Height);
+            }
+            else
+            {
+                int height = si.Height * sp.Width / si.Width;
+                int top = (sp.Height - height) / 2;
+                return new Rectangle(0, top, sp.Width, height);
+            }
+        }
+        private void ConvertPictureBoxCoordinates(PictureBox box, out int targetX, out int targetY, int srcX, int srcY)
+        {
+            int boxWidth = box.ClientSize.Width;
+            int boxHeight = box.ClientSize.Height;
+            int imgwidth = box.Image.Width;
+            int imgHeight = box.Image.Height;
+
+            targetX = srcX;
+            targetY = srcY;
+
+            switch (box.SizeMode)
+            {
+                case PictureBoxSizeMode.AutoSize:
+                case PictureBoxSizeMode.Normal:
+                case PictureBoxSizeMode.CenterImage:
+                case PictureBoxSizeMode.Zoom:
+                    break;
+                case PictureBoxSizeMode.StretchImage:
+                    targetX = (int)(imgwidth * srcX / (float)boxWidth);
+                    targetY = (int)(imgHeight * srcY / (float)boxHeight);
+                    break;
+            }
+
+        }
+        private void GetImgPixelRGBValue(out int red, out int green, out int blue, int x1, int y1)
+        {
+            //TypeSpecificMat (faster)
+            var mat3 = new Mat<Vec3b>(image);
+            var indexer = mat3.GetIndexer();
+            Vec3b color = indexer[y1, x1];
+            byte temp = color.Item0;
+            color.Item0 = color.Item2;
+            color.Item2 = temp;
+            indexer[y1, x1] = color;
+
+            red = color.Item0;
+            green = color.Item1;
+            blue = color.Item2;
+        }
+
+        private void GetImgPixelGrayValue(out int gray, int x1, int y1)
+        {
+            var mat1 = new Mat<Byte>(image);
+            var indexer = mat1.GetIndexer();
+            gray = indexer[y1, x1];
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                LoadImage(dlg.FileName);
+            }
+        }
+
+        private void LoadImage(String fileName)
+        {
+            image = Cv2.ImRead(fileName, ImreadModes.Unchanged);
+            imageType = image.Type();
+            pictureBoxImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+            ImageLoaded = true;
+
+        }
+    }
+}
